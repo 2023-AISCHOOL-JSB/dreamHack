03876e7bb3e59f32fac7b38d3b23fb5a0b7a351a
kyw <kyw4265@gmail.com>
2023-08-05 14:29:34 +0900

    8/5

---
+++ public/assets/js/todolist.js
@@ -0,0 +1,6 @@
+let title = document.querySelector("#todo__title")
+let writer = document.querySelector("#todo__writer")
+let pw = document.querySelector("#todo__pw")
+let content = document.querySelector("#todo__content")
+
+title.innerText = 
