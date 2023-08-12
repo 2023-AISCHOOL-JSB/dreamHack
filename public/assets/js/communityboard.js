fb37372612653bd096ea2942218b5a7956b2bf71
jaehyun <kjh47515719@gmail.com>
2023-08-12 12:19:23 +0900

    djlaf

--- public/assets/js/communityboard.js
+++ public/assets/js/communityboard.js
@@ -1,3 +1,18 @@
+let com_myForm = document.getElementById("myForm");
+com_myForm.addEventListener("submit",function(e){
+    let title = document.getElementById("com_title");
+    if(title.value.length == 0){
+        alert("제목을 입력하세요")
+        e.preventDefault();
+    }
+
+    let content = document.getElementById("com_content");
+    if(content.value.length == 0){
+        alert("내용을 입력하세요")
+        e.preventDefault();
+    }
+})
+
 
 
 
